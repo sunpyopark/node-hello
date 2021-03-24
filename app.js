e282df752129d4070d3f6c0d74da98c1ef9519ab
sunpyopark <59463698+sunpyopark@users.noreply.github.com>
2021-03-23 19:35:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,14 @@
-var http=require('http');
-var url=require('url');
-var js2xmlparser = require("js2xmlparser");
+const http = require('http');
 
-var server=http.createServer(function(req,res){
+const hostname = '0.0.0.0';
+const port = 3000;
 
-    var url_parts = url.parse(req.url, true);
-    var pathname = url_parts.pathname;
-    var querystring = url_parts.query;
-    console.log('querystring: ', querystring);
-    console.log(req.headers);
-    console.log("1");
-//    res.writeHead(200, {'Content-Type': 'text/plain'});
-//    res.writeHead(200, {
-//        'X-Powered-By': 'bacon'
-//    });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
 
-    switch(pathname){
-        case '/jsonresult/json':
-            res.setHeader('Content-Type', 'application/json');
-            var otherArray = ["item1", "item2"];
-            var otherObject = { item1: "item1val", item2: "item2val" };
-            var json = JSON.stringify({
-              anObject: otherObject,
-              anArray: otherArray,
-              another: "item"
-            });
-            res.end(json);
-        break;
-        case '/xmlresult/xml':
-            res.setHeader('Content-Type', 'application/xml');
-            var otherObject = { item1: "item1val", item2: "item2val" };
-            var xml = js2xmlparser.parse("person", otherObject);
-            console.log(xml);
-            res.end(xml);
-        break;
-        default:
-            res.end('plaintextresult');
-        break;
-    }
-
-}).listen(3001,"0.0.0.0");
-
-console.log('Server running at http://0.0.0.0:3001/');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
